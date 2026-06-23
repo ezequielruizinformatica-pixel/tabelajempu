faff60eeb856f041e77b09c08a0739dca421dd07
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-06-23 12:33:38 -0300

    Add image slideshow functionality in galeria.js

--- galeria.js
+++ galeria.js
@@ -1,1 +1,24 @@
+const imagens = [
 
+"imagens/foto1.jpg",
+"imagens/foto2.jpg",
+"imagens/foto3.jpg",
+"imagens/foto4.jpg"
+
+];
+
+let atual=0;
+
+setInterval(()=>{
+
+atual++;
+
+if(atual>=imagens.length){
+
+atual=0;
+
+}
+
+document.getElementById("slide").src=imagens[atual];
+
+},5000);
