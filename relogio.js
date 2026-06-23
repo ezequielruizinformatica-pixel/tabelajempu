bf578b65cd5ff0e8ec7baecd79e28e5668f47a94
ezequielruizinformatica-pixel <ezequielruizinformatica@gmail.com>
2026-06-23 12:33:13 -0300

    Add clock update function with interval

--- relogio.js
+++ relogio.js
@@ -1,1 +1,12 @@
+function atualizarRelogio(){
 
+const agora = new Date();
+
+document.getElementById("relogio").innerHTML =
+agora.toLocaleTimeString("pt-BR");
+
+}
+
+setInterval(atualizarRelogio,1000);
+
+atualizarRelogio();
